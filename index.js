c8a0ef1a5b955ab6c363260bffff916c620a9294
TylerL <tluckewicz@gmail.com>
2021-03-19 11:13:14 -0400

    Finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('ID', 'victory');
+
+newHeader.textContent = 'Tyler is the champion';
+
+document.body.appendChild(newHeader);
